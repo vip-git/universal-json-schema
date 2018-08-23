b6dd5d241772a57e4222bfb8798ef5beaf9ab7d5
vipin <6302771+vip-git@users.noreply.github.com>
2018-08-23 09:48:15 +0200

    fix: ability to customize value options

--- src/fields/configure/values-to-options.js
+++ src/fields/configure/values-to-options.js
@@ -2,7 +2,9 @@ import keys from 'lodash/keys';
 
 export default (values) => {
   if (values instanceof Array) {
-    return values.map(e => ({ key: e, value: e }));
+    return values.map(e => {
+      return (typeof e === 'object') ? ({ key: e.key, value: e.value }) : ({ key: e, value: e });
+    });
   }
   if (typeof values === 'object') {
     return keys(values).map(e => ({ key: e, value: values[e] }));
