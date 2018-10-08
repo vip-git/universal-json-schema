4135f64965ee16ec7fbe59cc506bee78323936ef
Vipin <6302771+vip-git@users.noreply.github.com>
2018-10-08 10:58:03 +0200

    fix: re-ordering fixed

--- src/FieldSet/ReorderControls.jsx
+++ src/FieldSet/ReorderControls.jsx
@@ -8,8 +8,9 @@ import fieldSetStyles from './field-set-styles';
 
 export const RawReorderControls = ({ first, last, classes, onMoveItemUp, onMoveItemDown, onDeleteItem }) => (
   <div className={classes.root}>
+    {/* Needs improvement - we will enable this later in stage !
     <IconButton className={classes.up} onClick={onMoveItemUp} disabled={first}><ArrowUpward /></IconButton>
-    <IconButton className={classes.down} onClick={onMoveItemDown} disabled={last}><ArrowDownward /></IconButton>
+    <IconButton className={classes.down} onClick={onMoveItemDown} disabled={last}><ArrowDownward /></IconButton> */}
     <IconButton className={classes.remove} onClick={onDeleteItem} ><RemoveCircle /></IconButton>
   </div>
 );
