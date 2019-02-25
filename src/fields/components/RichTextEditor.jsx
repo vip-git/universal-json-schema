eb5fe941fa3060dbca2f51b590361da1f2e2486f
vipin <6302771+vip-git@users.noreply.github.com>
2019-02-25 16:57:39 +0100

    fix: rich text class added for label

--- src/fields/components/RichTextEditor.jsx
+++ src/fields/components/RichTextEditor.jsx
@@ -3,14 +3,16 @@ import RichTextEditor from './lib/RichText';
 
 export default ({ type, value, options, label, htmlid, nullOption, onChange, ...rest }) => (
     <div>
-        <div style={{
-            'color': 'rgba(0, 0, 0, 0.54)',
-            'padding': 0,
-            'fontSize': '0.8rem',
-            'fontFamily': '"Roboto", "Helvetica", "Arial", sans-serif',
-            'lineHeight': 1,
-            'marginBottom': 5,
-        }}
+        <div 
+            className={'richTextLabel'}
+            style={{
+                'color': 'rgba(0, 0, 0, 0.54)',
+                'padding': 0,
+                'fontSize': '0.8rem',
+                'fontFamily': '"Roboto", "Helvetica", "Arial", sans-serif',
+                'lineHeight': 1,
+                'marginBottom': 5,
+            }}
         > { label } 
         </div>
         <RichTextEditor htmlid={htmlid} value={String(value)} onChange={onChange} {...rest} />
