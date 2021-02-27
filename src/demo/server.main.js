e8bca1604da661e1209ea386fdda208541eb4376
Vipin <6302771+vip-git@users.noreply.github.com>
2021-02-27 18:39:31 +0100

    fix: publishing custom packages now possible

--- src/demo/server.main.js
+++ src/demo/server.main.js
@@ -1,10 +1,13 @@
+/* eslint-disable max-len */
 /* eslint-disable global-require,import/no-extraneous-dependencies */
 const express = require('express');
 const path = require('path');
+const { generate } = require('shortid');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 
 const port = process.env.PORT || 3005;
+const tempStorage = {};
 
 const app = express();
 app.use(bodyParser.json());
@@ -53,13 +56,7 @@ app.post('/create_components', (req, res) => {
   );
 
   if (req.body && req.body.sessionId) {
-    if (!fs.existsSync(`generator/${req.body.sessionId}`)) {
-      fs.mkdirSync(`generator/${req.body.sessionId}`);
-    }
-    fs.writeFileSync(
-      `generator/${req.body.sessionId}/components.json`,
-      JSON.stringify(componentsJSON, null, 2),
-    );
+    tempStorage[req.body.sessionId] = componentsJSON;
     return res.status(200).send(req.body);
   }
 
@@ -87,13 +84,126 @@ app.post('/publish_package', (req, res) => {
       }
       fs.rmdirSync(dir);
     };
-    const packageJSON = require('../../package.json');
-    packageJSON.name = req.body.packageName;
-    packageJSON.version = req.body.packageVersion;
+    const componentsInfo = tempStorage[req.body.sessionId];
+    if (!fs.existsSync(`generator/${req.body.sessionId}`)) {
+      fs.mkdirSync(`generator/${req.body.sessionId}`);
+    }
+    fs.writeFileSync(
+      `generator/${req.body.sessionId}/components.json`,
+      JSON.stringify(componentsInfo, null, 2),
+    );
+    const packageInfo = require('../../package.json');
+    const packageName = `${req.body.packageName}-${generate().toLowerCase()}`;
+    packageInfo.name = `@react-jsonschema-form-custom/${packageName}`;
+    packageInfo.author = 'React JSON Schema Bot';
+    packageInfo.version = req.body.packageVersion;
+    delete packageInfo.scripts.postinstall;
     fs.writeFileSync(
       `generator/${req.body.sessionId}/package.json`,
-      JSON.stringify(packageJSON, null, 2),
+      JSON.stringify(packageInfo, null, 2),
     );
+
+    const readMeTemplate = `# React Json Schema Form Custom (${packageName})
+>[**Material UI**](http://www.material-ui.com/) port of [**jsonschema-form.**](https://json-schema.org/)
+
+>[**live playground**](https://react-jsonschema-form-material-ui.github56.now.sh/) and [**Detailed Docs**](https://react-json-schema.app/docs)
+
+## Install instructions via npm
+
+\`\`\`text
+npm install --save ${packageInfo.name}
+\`\`\`
+
+## Example Usage
+
+> More detailed example can be seen [here](https://github.com/vip-git/react-jsonschema-form-material-ui/blob/master/src/demo/body/Example.jsx)
+
+\`\`\`jsx
+// Library
+import React from 'react';
+import MaterialJsonSchemaForm from '${packageInfo.name}';
+
+// Internals
+const schema = require('./path-to your-schema.json');
+const uiSchema = require('./path-to your-ui-schema.json');
+const formData = require('./path-to your-ui-formData.json');
+
+const Example () => {
+    const onSubmit = (value, callback) => {
+        console.log('onSubmit: %s', JSON.stringify(value)); // eslint-disable-line no-console
+        setTimeout(() => callback && callback(), 2000); // just an example in real world can be your XHR call
+    }
+    
+    const onCancel = () => {
+        console.log('on reset being called');
+    }
+    
+    const onFormChanged = ({ formData }) => {
+        console.log('onFormChanged: ',formData); // eslint-disable-line no-console
+    }
+    
+    const onUpload = (value) => {
+        console.log('onUpload: ', value); // eslint-disable-line no-console
+    }
+    
+    return (
+      <MaterialJsonSchemaForm
+        schema={givenSchema}
+        uiSchema={givenUISchema}
+        formData={givenFormData}
+        onCancel={onCancel}
+        onSubmit={onSubmit}
+        onUpload={onUpload}
+        onChange={onFormChanged}
+        onError={onError}
+        /* Optional Param for custom functions to be executed for transforming data */
+        interceptors={{
+            translateRatings: (givenData, uiData) => ({ givenData, uiData }),
+        }}
+        /* Optional Param for custom components */
+        components={{
+        customComponent: ({ onChange, ...rest }) => (
+          <CustomComponent onChange={onChange} formData={givenFormData} uiData={givenUIData} {...rest} />
+        ),
+        customRating: ({ onChange, ...rest }) => (
+          <CustomRating onChange={onChange} formData={givenFormData} uiData={givenUIData} {...rest} />
+        ),
+        }}
+        /* Optional Param for custom validation */
+        validations={{
+            confirmPassword: ({ schema, validations, formData, value }) => value !== formData.pass1 && ({
+            message: validations.confirmPassword.message,
+            inline: true,
+            }),
+        }}
+        /* Optional Param to auto submit form on press of enter */
+        submitOnEnter
+      />
+    );
+}
+
+export default Example;
+\`\`\`
+    `;
+    fs.writeFile(
+      `generator/${req.body.sessionId}/README.md`,
+      readMeTemplate,
+      () => {},
+    );
+
+    /** Todo: replace this with environment variable */
+    const npmrcTemplate = `
+      //registry.npmjs.org/:_authToken=df919533-d8b7-42c3-bb92-0b380aabc35a
+      registry=https://registry.npmjs.org/
+      always-auth=true
+    `;
+
+    fs.writeFile(
+      `generator/${req.body.sessionId}/.npmrc`,
+      npmrcTemplate,
+      () => {},
+    );
+
     const dockerFileTemplate = `
 # Use an official node image
 FROM node:lts-alpine
@@ -110,9 +220,12 @@ RUN mkdir -p /opt/react-json-schema
 WORKDIR /opt/react-json-schema
 COPY src/ /opt/react-json-schema/src
 COPY .babelrc /opt/react-json-schema/.babelrc
+COPY .npmignore /opt/react-json-schema/.npmignore
 COPY generator/ /opt/react-json-schema/generator
 COPY generator/${req.body.sessionId}/components.json /opt/react-json-schema/generator/components.json
 COPY generator/${req.body.sessionId}/package.json /opt/react-json-schema/package.json
+COPY generator/${req.body.sessionId}/README.md /opt/react-json-schema/README.md
+COPY generator/${req.body.sessionId}/.npmrc /opt/react-json-schema/.npmrc
 COPY index.js /opt/react-json-schema/index.js
 COPY webpack.config.js /opt/react-json-schema/webpack.config.js
 
@@ -139,8 +252,8 @@ RUN npm link webpack && \\
     npm link @babel/runtime
 RUN node generator/index.js
 RUN npx webpack
-# RUN npm login --username --password 
-# RUN npm publish --access public 
+RUN npm version
+RUN npm publish --access public 
 
 CMD ["node", "index.js"]
     `;
@@ -162,7 +275,11 @@ CMD ["node", "index.js"]
         }
       },
     );
-    return res.status(200).send(req.body);
+    return res.status(200).send({
+      packageInfo,
+      componentsInfo,
+      readMeTemplate,
+    });
   }
   return res.status(500).send({ error: 'Invalid Session' });
 });
