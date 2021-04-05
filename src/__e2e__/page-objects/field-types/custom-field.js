c15daaf697656521727bce18feae6ae4c02b7078
Vipin <6302771+vip-git@users.noreply.github.com>
2021-04-05 12:50:35 +0200

    fix: issues resolved with custom component verification clicks

--- src/__e2e__/page-objects/field-types/custom-field.js
+++ src/__e2e__/page-objects/field-types/custom-field.js
@@ -1,12 +1,6 @@
 const _ = require('lodash');
 
-const execute = ({
-  hasTestsSchema,
-  getRefrencePointer,
-  callbackBeforeCompare,
-  fieldUIType,
-}) => {
-  const uiTestDef = _.get(hasTestsSchema, getRefrencePointer);
+const validateTest = ({ uiTestDef, callbackBeforeCompare, fieldUIType }) => {
   if (
     uiTestDef.steps &&
     Array.isArray(uiTestDef.steps) &&
@@ -14,19 +8,51 @@ const execute = ({
   ) {
     uiTestDef.steps.forEach((stepDef) => {
       const path = _.get(uiTestDef, stepDef.selector);
+      const fieldValue = $(path).getValue() || $(path).getText();
       switch (stepDef.action) {
         case 'click':
-          $(path).click();
+            if (stepDef.verify) {
+                const { action, value: newValue, selector: verifySelector } = stepDef.verify;
+                const selector = _.get(uiTestDef, verifySelector);
+                const verifyFieldValue = $(selector).getValue() || $(selector).getText();;
+                if (newValue && verifyFieldValue.includes(newValue) && action) {
+                  $(selector).click();
+                }
+            }
+            $(path).click();
           return;
         case 'compare':
           callbackBeforeCompare(fieldUIType);
-          const fieldValue = $(path).getValue() || $(path).getText();
           expect(stepDef.value).toContain(fieldValue);
           return;
       }
     });
   } else {
     expect('No Tests defined for custom components').toBe(getRefrencePointer);
+  }
+};
+
+const execute = ({
+  hasTestsSchema,
+  getRefrencePointer,
+  callbackBeforeCompare,
+  fieldUIType,
+}) => {
+  const uiTestDefs = _.get(hasTestsSchema, getRefrencePointer);
+  if (uiTestDefs && Array.isArray(uiTestDefs)) {
+    uiTestDefs.forEach((uiTestDef) => {
+      validateTest({ 
+          uiTestDef, 
+          callbackBeforeCompare, 
+          fieldUIType 
+      });
+    });
+  } else {
+      validateTest({
+        uiTestDef: uiTestDefs,
+        callbackBeforeCompare,
+        fieldUIType,
+      });
   }
 };
 
