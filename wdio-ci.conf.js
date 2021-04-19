3bbdc017b8cee7e3de81eb8831255c03f24a4994
Vipin Tanna <6302771+vip-git@users.noreply.github.com>
2021-04-19 08:51:55 +0200

    Update wdio-ci.conf.js

--- wdio-ci.conf.js
+++ wdio-ci.conf.js
@@ -46,7 +46,7 @@ exports.config = {
   // NPM script (see https://docs.npmjs.com/cli/run-script) then the current working
   // directory is where your package.json resides, so `wdio` will be called from there.
   //
-  specs: ['./src/demo/examples/**/stepper.feature'],
+  specs: ['./src/demo/examples/**/*.feature'],
   // Patterns to exclude.
   exclude: [
     // 'path/to/excluded/files'
@@ -104,7 +104,7 @@ exports.config = {
   // Define all options that are relevant for the WebdriverIO instance here
   //
   // Level of logging verbosity: trace | debug | info | warn | error | silent
-  logLevel: 'info',
+  logLevel: 'silent',
   //
   // Set specific log levels per logger
   // loggers:
