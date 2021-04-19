5dabb53d743aa47230563d68d9520798cb38f4cf
Vipin <6302771+vip-git@users.noreply.github.com>
2021-04-20 01:17:01 +0200

    fix: mroe tests passing

--- wdio.conf.js
+++ wdio.conf.js
@@ -24,7 +24,7 @@ exports.config = {
   // NPM script (see https://docs.npmjs.com/cli/run-script) then the current working
   // directory is where your package.json resides, so `wdio` will be called from there.
   //
-  specs: ['./src/demo/examples/**/single.feature'],
+  specs: ['./src/demo/examples/**/tabs-*.feature'],
   // Patterns to exclude.
   exclude: [
     // 'path/to/excluded/files'
