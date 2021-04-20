f2d32b8de56badf82a28a7e995fec63673d20fd2
Vipin <6302771+vip-git@users.noreply.github.com>
2021-04-20 22:36:47 +0200

    fix: issues resolved

--- wdio.conf.js
+++ wdio.conf.js
@@ -97,7 +97,7 @@ exports.config = {
   // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
   // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
   // gets prepended directly.
-  baseUrl: 'http://localhost:3005',
+  baseUrl: 'https://react-jsonschema-form-material-ui-gk7oou93c-github56.vercel.app',
   //
   // Default timeout for all waitFor* commands.
   waitforTimeout: 10000,
