56f44875b2f003eee5d992ddfc204d120e214c85
Vipin <6302771+vip-git@users.noreply.github.com>
2021-04-24 14:17:52 +0200

    docs: on adding safari tests locally updated

--- wdio.conf.js
+++ wdio.conf.js
@@ -65,6 +65,18 @@ exports.config = {
       // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
       // excludeDriverLogs: ['bugreport', 'server'],
     },
+    // {
+    //   // maxInstances can get overwritten per capability. So if you have an in-house Selenium
+    //   // grid with only 5 firefox instances available you can make sure that not more than
+    //   // 5 instances get started at a time.
+    //   maxInstances: 5,
+    //   // run safaridriver -p 4444 before enabling this
+    //   browserName: 'Safari',
+    //   // If outputDir is provided WebdriverIO can capture driver session logs
+    //   // it is possible to configure which logTypes to include/exclude.
+    //   // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
+    //   // excludeDriverLogs: ['bugreport', 'server'],
+    // },
   ],
   //
   // ===================
