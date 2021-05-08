e9aca25569dcfa69dda02ee7309dc67b7295b86d
Vipin Tanna <6302771+vip-git@users.noreply.github.com>
2021-05-08 21:48:05 +0200

    Update wdio-sauce-ci.conf.js

--- wdio-sauce-ci.conf.js
+++ wdio-sauce-ci.conf.js
@@ -295,7 +295,7 @@ exports.config = {
     // <string> (expression) only execute the features or scenarios with tags matching the expression
     tagExpression: '',
     // <number> timeout for step definitions
-    timeout: 60000,
+    timeout: 960000,
     // <boolean> Enable this config to treat undefined definitions as warnings.
     ignoreUndefinedDefinitions: false,
   },
