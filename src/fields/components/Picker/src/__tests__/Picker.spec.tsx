d7d54e3542d31e9decb795cbf2c027280b660e41
Vipin <6302771+vip-git@users.noreply.github.com>
2021-07-10 22:23:29 +0200

    unit: more tests for picker component

--- src/fields/components/Picker/src/__tests__/Picker.spec.tsx
+++ src/fields/components/Picker/src/__tests__/Picker.spec.tsx
@@ -1,4 +1,5 @@
 // Library
+import moment from 'moment';
 import React from 'react';
 import { mount } from 'enzyme';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
@@ -17,20 +18,50 @@ const schema: JSONSchema7 = {
 const value = '2010-10-10';
 
 const uiDateTypes = [
-  { type: 'material-date', defaultValue: '10-10-2010' },
-  { type: 'material-date-keyboard-disable', defaultValue: '10-10-2010' },
-  { type: 'material-time-keyboard-disable', defaultValue: '12:00 AM' },
-  { type: 'material-time', defaultValue: '12:00 AM' },
-  { type: 'material-datetime', defaultValue: '10-10-2010 12:00 AM' },
-  { type: 'material-datetime-keyboard-disable', defaultValue: '10-10-2010 12:00 AM' },
+  { type: 'material-date', defaultValue: '10-10-2010', changeValue: '2010-10-10' },
+  { type: 'material-date-keyboard-disable', defaultValue: '10-10-2010', changeValue: '2010-10-10' },
+  { type: 'material-time-keyboard-disable', defaultValue: '12:00 AM', changeValue: '2010-10-10' },
+  { type: 'material-time', defaultValue: '12:00 AM', changeValue: '12:00 AM' },
+  { type: 'material-datetime', defaultValue: '10-10-2010 12:00 AM', changeValue: '2010-10-10' },
+  { type: 'material-datetime-keyboard-disable', defaultValue: '10-10-2010 12:00 AM', changeValue: '2010-10-10' },
 ];
 
 describe('Picker', () => {
-  uiDateTypes.forEach(({ type, defaultValue }) => {
+  uiDateTypes.forEach(({ type, defaultValue, changeValue }) => {
     it(`mounts with standard attributes (${type})`, () => {
       const path = 'done'; 
       const label = 'Done';
       schema.description = label;
+      const wrapper2 = mount(
+        <MuiPickersUtilsProvider utils={MomentUtils}>
+          <PickerComp 
+            label={label}
+            path={path}
+            value={value}
+            schema={schema}
+            uiSchema={{ 'ui:widget': type, 'ui:props': {} }}
+            title={label}
+            htmlid={'test'}
+            type={type}
+            onChange={jest.fn}
+          />
+        </MuiPickersUtilsProvider>,
+      );
+      const wrapper3 = mount(
+        <MuiPickersUtilsProvider utils={MomentUtils}>
+          <PickerComp 
+            label={label}
+            path={path}
+            value={undefined}
+            schema={schema}
+            uiSchema={{ 'ui:widget': type, 'ui:props': {} }}
+            title={label}
+            htmlid={'test'}
+            type={type}
+            onChange={jest.fn}
+          />
+        </MuiPickersUtilsProvider>,
+      );
       const wrapper = mount(
         <MuiPickersUtilsProvider utils={MomentUtils}>
           <PickerComp 
@@ -38,7 +69,7 @@ describe('Picker', () => {
             path={path}
             value={value}
             schema={schema}
-            uiSchema={{ 'ui:widget': type }}
+            uiSchema={{ 'ui:widget': type, 'ui:props': { variant: type} }}
             title={label}
             htmlid={'test'}
             type={type}
@@ -54,55 +85,55 @@ describe('Picker', () => {
       expect(cbComp).toHaveLength(1);
       expect(cbComp.prop('value')).toBe(defaultValue);
     });
-  })
 
-  it('passes additional properties to the Checkbox component', () => {
-    const props = {
-      color: 'secondary',
-    }
-    const wrapper = mount(
-      <MuiPickersUtilsProvider utils={MomentUtils}>
-        <PickerComp 
-          label={'label'}
-          path={'path'}
-          value={value}
-          schema={schema}
-          title={'label'}
-          htmlid={'test'}
-          type={'string'}
-          onChange={jest.fn}
-          options={
-            {...props} 
-          }
-        />
-      </MuiPickersUtilsProvider>,
-    );
+    it(`passes additional properties to the ${type} component`, () => {
+      const props = {
+        color: 'secondary',
+      }
+      const wrapper = mount(
+        <MuiPickersUtilsProvider utils={MomentUtils}>
+          <PickerComp 
+            label={'label'}
+            path={'path'}
+            value={value}
+            schema={schema}
+            title={'label'}
+            htmlid={'test'}
+            type={'string'}
+            onChange={jest.fn}
+            options={
+              {...props} 
+            }
+          />
+        </MuiPickersUtilsProvider>,
+      );
 
-    const cbComp = wrapper.find('WithStyles(ForwardRef(TextField))');
-    expect(cbComp.prop('color')).toBe(props.color);
-  });
-
-  it('calls onChange when clicked', () => {
-    const onChange = jest.fn();
-    const wrapper = mount(
-      <MuiPickersUtilsProvider utils={MomentUtils}>
-        <PickerComp 
-          path={'a'}
-          value={value}
-          onChange={onChange}
-          schema={schema}
-          label={'label'}
-          title={'label'}
-          htmlid={'test'}
-          type={'string'}
-        />
-      </MuiPickersUtilsProvider>,
-    );
-    const cbComp = wrapper.find('WithStyles(ForwardRef(TextField))');
-    expect(cbComp).toHaveLength(1);
-    cbComp.prop('onChange')({
-      target: { value: '2020-10-10' }
+      const cbComp = wrapper.find('WithStyles(ForwardRef(TextField))');
+      expect(cbComp.prop('color')).toBe(props.color);
     });
-    expect(onChange).toHaveBeenCalledTimes(1);
+
+    it(`calls onChange when clicked (${type})`, () => {
+      const onChange = jest.fn();
+      const wrapper = mount(
+        <MuiPickersUtilsProvider utils={MomentUtils}>
+          <PickerComp 
+            path={'a'}
+            value={value}
+            onChange={onChange}
+            schema={schema}
+            label={'label'}
+            title={'label'}
+            htmlid={'test'}
+            type={'string'}
+          />
+        </MuiPickersUtilsProvider>,
+      );
+      const cbComp = wrapper.find('WithStyles(ForwardRef(TextField))');
+      expect(cbComp).toHaveLength(1);
+      cbComp.prop('onChange')({
+        target: { value: changeValue }
+      });
+      expect(onChange).toHaveBeenCalledTimes(1);
+    });
   });
 });
