52e955035516b3675a173d1cb5b3b2ead3d65d99
Vipin <6302771+vip-git@users.noreply.github.com>
2021-07-12 00:07:29 +0200

    unit: more tests for interceptors

--- src/fields/interceptors/translate-currency/src/__tests__/translate-curreny.spec.ts
+++ src/fields/interceptors/translate-currency/src/__tests__/translate-curreny.spec.ts
@@ -2,7 +2,7 @@
 import translateCurrency from '..';
 
 describe('translateCurrency', () => {
-    it('returns formatted currency with original value', () => {
+    it('returns formatted currency with original value (us)', () => {
       const value = '121231.23';
       const expected = {
         'formData': 121231.23, 
@@ -12,6 +12,36 @@ describe('translateCurrency', () => {
           value,
           options: {
             useLocaleString: 'us'
+          }
+      });
+      expect(actual).toStrictEqual(expected);
+    });
+
+    it('returns formatted currency with original value (fr)', () => {
+      const value = '121231,23';
+      const expected = {
+        'formData': 121231.23, 
+        'uiData': '121 231,23'
+      };
+      const actual = translateCurrency({
+          value,
+          options: {
+            useLocaleString: 'fr'
+          }
+      });
+      expect(actual).toStrictEqual(expected);
+    });
+
+    it('returns formatted currency with original value (de)', () => {
+      const value = '121231,23';
+      const expected = {
+        'formData': 121231.23, 
+        'uiData': '121.231,23'
+      };
+      const actual = translateCurrency({
+          value,
+          options: {
+            useLocaleString: 'de'
           }
       });
       expect(actual).toStrictEqual(expected);
