433b63bf16680e9448fb9a236bdc8ff1c32b79d6
Vipin <6302771+vip-git@users.noreply.github.com>
2021-07-12 21:16:50 +0200

    unit: more tests for helpers

--- src/helpers/__tests__/execute-xhr.spec.ts
+++ src/helpers/__tests__/execute-xhr.spec.ts
@@ -2,16 +2,45 @@ import executeXHRCall from '../execute-xhr-call';
 
 describe('executeXHRCall', () => {
   it('can execute initial call', async () => {
+    const onSuccess = jest.fn();
+    const onFailure = jest.fn();
     // assemble
     const data = {
-      type: 'string',
+      type: 'load',
+      url: 'https://asdasd',
+      method: 'GET',
+      payload: [],
+      onSuccess,
+      onFailure,
     };
-    const expected = false;
+    const expected = undefined;
 
     // act
     const actual = await executeXHRCall(data);
 
     // assert
     expect(actual).toEqual(expected);
+    expect(onFailure).toHaveBeenCalledTimes(1);
+  });
+  it('can execute initial call (POST)', async () => {
+    const onSuccess = jest.fn();
+    const onFailure = jest.fn();
+    // assemble
+    const data = {
+      type: 'load',
+      url: 'https://asdasd',
+      method: 'POST',
+      payload: [],
+      onSuccess,
+      onFailure,
+    };
+    const expected = undefined;
+
+    // act
+    const actual = await executeXHRCall(data);
+
+    // assert
+    expect(actual).toEqual(expected);
+    expect(onFailure).toHaveBeenCalledTimes(1);
   });
 });
