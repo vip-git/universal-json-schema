00d70d48a514bd02af171e7569b1f16a6f1ed1fb
Vipin <6302771+vip-git@users.noreply.github.com>
2021-07-12 21:38:14 +0200

    unit: more tests for form state

--- src/helpers/state-machine/form/__tests__/form-state-guards.spec.ts
+++ src/helpers/state-machine/form/__tests__/form-state-guards.spec.ts
@@ -2,12 +2,21 @@
 import GUARDS from '../form-state.guards';
 
 describe('Form State Guards', () => {
-    it('Should be able to get states', () => {
+    it('Should be able to Update Field', () => {
         const fieldStates = GUARDS.isUpdatedField('test')({}, {
             field: 'test',
             givenValue: 'hello'
         }, {});
         const expected = 'true';
+        expect(JSON.stringify(fieldStates)).toBe(expected);
+    });
+
+    it('Should be able to Update Error Field', () => {
+        const fieldStates = GUARDS.isUpdatedErrorField('test')({}, {
+            dataPath: 'test',
+            params: { missingProperty: 'hello' }
+        });
+        const expected = 'false';
         expect(JSON.stringify(fieldStates)).toBe(expected);
     });
 });
