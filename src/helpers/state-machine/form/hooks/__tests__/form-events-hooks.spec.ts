00c0036d36f3ce99f8fbd2c01b514be1c940ca17
Vipin <6302771+vip-git@users.noreply.github.com>
2021-07-14 19:59:54 +0200

    unit: more tests for form event hooks

--- src/helpers/state-machine/form/hooks/__tests__/form-events-hooks.spec.ts
+++ src/helpers/state-machine/form/hooks/__tests__/form-events-hooks.spec.ts
@@ -3,16 +3,65 @@ import useFormEvents from '../form-events.hooks';
 
 describe('useFormEvents', () => {
     [
-        'onMoveItemDown',
-        'onDeleteItem',
-        'onAddItem',
-        'onAddNewProperty',
-        'onRemoveProperty',
-        'onUpdateKeyProperty',
-        'onFormValuesChange',
-        'onXHRSchemaEvent',
+        { 
+            name: 'onMoveItemDown',
+            fn: (formEvents) => formEvents['onMoveItemDown']('test.me', 1)('hello', 'world'),
+            spy: (params) => jest.spyOn(params.stateMachineService, 'send')
+        },
+        { 
+            name: 'onDeleteItem',
+            fn: (formEvents) => formEvents['onDeleteItem']('test.me', 1)('hello', 'world'),
+            spy: (params) => jest.spyOn(params.stateMachineService, 'send')
+        },
+        { 
+            name: 'onAddItem',
+            fn: (formEvents) => formEvents['onAddItem']('test.me', 1)('hello', 'world'),
+            spy: (params) => jest.spyOn(params.stateMachineService, 'send')
+        },
+        { 
+            name: 'onAddNewProperty',
+            fn: (formEvents) => formEvents['onAddNewProperty']('test.me', 1)('hello', 'world'),
+            spy: (params) => jest.spyOn(params.stateMachineService, 'send')
+        },
+        { 
+            name: 'onRemoveProperty',
+            fn: (formEvents) => formEvents['onRemoveProperty']('test.me', 1)('hello', 'world'),
+            spy: (params) => jest.spyOn(params.stateMachineService, 'send')
+        },
+        { 
+            name: 'onUpdateKeyProperty',
+            fn: (formEvents) => formEvents['onUpdateKeyProperty']('test.me', 1)('hello', 'world'),
+            spy: (params) => jest.spyOn(params.stateMachineService, 'send') 
+        },
+        { 
+            name: 'onFormValuesChange',
+            fn: (formEvents) => formEvents['onFormValuesChange']('test.me', 1)('hello', 'world'),
+            spy: (params) => jest.spyOn(params.stateMachineService, 'send')
+        },
+        { 
+            name: 'onXHRSchemaEvent',
+            fn: (formEvents) => formEvents['onXHRSchemaEvent']('test.me', 1)('hello', 'world'),
+            spy: (params) => jest.spyOn(params.stateMachineService, 'send')
+        },
+        { 
+            name: 'onFormSubmit',
+            fn: (formEvents) => formEvents['onFormSubmit']('test.me', 1),
+            spy: (params) => jest.spyOn(params, 'onSubmit')
+        },
+        { 
+            name: 'handleKeyEnter',
+            fn: (formEvents) => formEvents['handleKeyEnter']({
+                keyCode: 13,
+            }),
+            spy: (params) => jest.spyOn(params, 'onSubmit')
+        },
+        { 
+            name: 'onTabChange',
+            fn: (formEvents) => formEvents['onTabChange']('test.me', 1),
+            spy: (params) => jest.spyOn(params.stateMachineService, 'send')
+        },
     ].map((actionName) => {
-        it(`Should be able execute ${actionName}`, () => {
+        it(`Should be able execute ${actionName.name}`, () => {
             const params = {
                 stateMachineService: {
                     send: jest.fn(),
@@ -24,12 +73,12 @@ describe('useFormEvents', () => {
                 uiSchema: {},
                 xhrSchema: {},
                 interceptors: {},
-                submitOnEnter: {},
-                onSubmit: {},
+                submitOnEnter: true,
+                onSubmit: jest.fn(),
             };
-            const sendSpy = jest.spyOn(params.stateMachineService, 'send');
             const formEvents = useFormEvents(params);
-            formEvents[actionName]('test.me', 1)('hello', 'world');
+            const sendSpy = actionName['spy'](params);
+            actionName['fn'](formEvents);
             expect(sendSpy).toHaveBeenCalledTimes(1);
         });
     })
