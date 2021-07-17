c66a9610412bad470c8328379b163148cfce823c
Vipin <6302771+vip-git@users.noreply.github.com>
2021-07-17 15:37:01 +0200

    unit: more tests for Form.tsx

--- src/__tests__/Form.spec.tsx
+++ src/__tests__/Form.spec.tsx
@@ -1,5 +1,67 @@
+// Library
+import React from 'react';
+import { mount } from 'enzyme';
+
+// Internal
+import Form from '../Form';
+
 describe('Form', () => {
-  it('Test it works well', () => {
-    expect(true).toBe(true);
+  it('Mount the form with basic fields', () => {
+    const path = 'name';
+    const onChange = jest.fn(() => 'onChangeFunc');
+    const schema = {
+      'type': 'string',
+      'title': 'First Name',
+    };
+
+    const uiSchema = {
+    };
+
+    const data = 'Bob';
+
+    // act
+    const wrapper = mount(
+      <Form 
+        schema={schema}
+        uiSchema={uiSchema}
+        formData={data}
+        prefixId={'test'}
+        onChange={({ formData }) => jest.fn(formData)}
+        onSubmit={(submittedData) => jest.fn(submittedData)}
+      />,
+    );
+
+    // check
+    expect(wrapper).toHaveLength(1);
+  });
+
+
+  it('Mount the form with basic fields (with error)', () => {
+    const path = 'name';
+    const onChange = jest.fn(() => 'onChangeFunc');
+    const schema = {
+      'type': 'string',
+      'title': 'First Name',
+    };
+
+    const uiSchema = {
+    };
+
+    const data = 1;
+
+    // act
+    const wrapper = mount(
+      <Form 
+        schema={schema}
+        uiSchema={uiSchema}
+        formData={data}
+        prefixId={'test'}
+        onChange={({ formData }) => jest.fn(formData)}
+        onSubmit={(submittedData) => jest.fn(submittedData)}
+      />,
+    );
+
+    // check
+    expect(wrapper).toHaveLength(1);
   });
 });
