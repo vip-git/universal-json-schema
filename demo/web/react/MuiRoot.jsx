6db07aa9cdae7e396e1edadad2d0113ac7d2dde9
Vipin <6302771+vip-git@users.noreply.github.com>
2021-09-19 01:40:46 +0200

    fix: all style issues resolved

--- demo/web/react/MuiRoot.jsx
+++ demo/web/react/MuiRoot.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'typeface-roboto'; // eslint-disable-line import/extensions
 import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/styles';
+import { ThemeProvider } from '@mui/material/styles';
 import { StyledEngineProvider } from '@mui/material/styles';
 import theme from './theme';
 
